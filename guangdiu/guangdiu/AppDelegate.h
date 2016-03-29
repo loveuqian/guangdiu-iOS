93d34f6898fe242004a789ab16254939ce486ed9
WangShengFeng <loveuqian@qq.com>
2016-03-29 17:50:27 +0800

    first init

---
+++ guangdiu/guangdiu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  guangdiu
+//
+//  Created by WangShengFeng on 16/3/29.
+//  Copyright © 2016年 WangShengFeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ guangdiu/guangdiu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  guangdiu
+//
+//  Created by WangShengFeng on 16/3/29.
+//  Copyright © 2016年 WangShengFeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
